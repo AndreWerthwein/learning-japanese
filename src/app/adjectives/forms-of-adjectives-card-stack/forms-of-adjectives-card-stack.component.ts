3003f32bfa25afdfe5ae3e14957861f7d8f7591b
André-Pascal Werthwein <andre.werthwein@gmx.de>
2023-01-11 18:54:54 +0100

    adding basic content for 'vocabulary-card-stack'- and 'forms-of-adjectives-card-stack'-component

--- src/app/adjectives/forms-of-adjectives-card-stack/forms-of-adjectives-card-stack.component.ts
+++ src/app/adjectives/forms-of-adjectives-card-stack/forms-of-adjectives-card-stack.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
 
+// interfaces
+import { iFormsOfAdjectivesCard } from '../forms-of-adjectives-card/forms-of-adjectives-card.model';
+
+// data
+import { NA_ADJECTIVES } from 'src/app/data/data_formsOfAdjectives';
+
 @Component({
   selector: 'forms-of-adjectives-card-stack',
   templateUrl: './forms-of-adjectives-card-stack.component.html',
   styleUrls: ['./forms-of-adjectives-card-stack.component.scss'],
 })
-export class FormsOfAdjectivesCardStack {}
+export class FormsOfAdjectivesCardStack {
+  adjectives: Array<iFormsOfAdjectivesCard> = NA_ADJECTIVES;
+}
--- src/app/vocabulary/vocabulary-card-stack/vocabulary-card-stack.component.ts
+++ src/app/vocabulary/vocabulary-card-stack/vocabulary-card-stack.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
 
+// interfaces
+import { iVocabularyCard } from '../vocabulary-card/vocabulary-card.model';
+
+// data
+import { VOCABULARY } from 'src/app/data/data_vocabulary';
+
 @Component({
   selector: 'vocabulary-card-stack',
   templateUrl: './vocabulary-card-stack.component.html',
   styleUrls: ['./vocabulary-card-stack.component.scss'],
 })
-export class VocabularyCardStack {}
+export class VocabularyCardStack {
+  vocabulary: Array<iVocabularyCard> = VOCABULARY;
+}
